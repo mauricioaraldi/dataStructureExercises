ca8467d91b07c60c7f780a0205e9af79616ad583
Mauricio Araldi <araldi.mauricio@gmail.com>
2022-11-14 13:48:50 +0100

    Added iteractive version of traversals in treeOrder

--- basic_data_structures/treeOrders.js
+++ basic_data_structures/treeOrders.js
@@ -46,9 +46,9 @@ const readLines = () => {
   });
 };
 
-function traverseByLevel(tree) {
+function iteractiveTraverseByLevel(tree) {
   if (!tree) {
-    return;
+    return [];
   }
 
   const print = [];
@@ -73,30 +73,76 @@ function traverseByLevel(tree) {
   return print;
 }
 
-function traverseInOrder(tree, nodeIndex = 0) {
-  if (!tree) {
-    return;
-  }
-
+function iteractiveTraverseInOrder(tree) {
+  const queue = [];
   const print = [];
-  const node = tree[nodeIndex];
 
-  if (node[1] !== -1) {
-    print.push(...traverseInOrder(tree, node[1]));
-  }
+  let node = tree[0];
 
-  print.push(node[0]);
+  while(queue.length || node) {
+    while(node) {
+      queue.push(node);
 
-  if (node[2] !== -1) {
-    print.push(...traverseInOrder(tree, node[2]));
+      node = tree[node[1]];
+    }
+
+    node = queue.pop();
+
+    print.push(node[0]);
+
+    node = tree[node[2]];
   }
 
   return print;
 }
 
-function traversePreOrder(tree, nodeIndex = 0) {
+function recursiveTraverseInOrder(tree, nodeIndex = 0) {
   if (!tree) {
-    return;
+    return [];
+  }
+
+  const print = [];
+  const node = tree[nodeIndex];
+
+  if (node[1] !== -1) {
+    print.push(...recursiveTraverseInOrder(tree, node[1]));
+  }
+
+  print.push(node[0]);
+
+  if (node[2] !== -1) {
+    print.push(...recursiveTraverseInOrder(tree, node[2]));
+  }
+
+  return print;
+}
+
+function iteractiveTraversePreOrder(tree) {
+  const queue = [tree[0]];
+  const print = [];
+
+  let node;
+
+  while(queue.length) {
+    node = queue.pop();
+
+    print.push(node[0]);
+
+    if(node[2] !== -1) {
+      queue.push(tree[node[2]]);
+    }
+
+    if(node[1] !== -1) {
+      queue.push(tree[node[1]]);
+    }
+  }
+
+  return print;
+}
+
+function recursiveTraversePreOrder(tree, nodeIndex = 0) {
+  if (!tree) {
+    return [];
   }
 
   const print = [];
@@ -105,30 +151,59 @@ function traversePreOrder(tree, nodeIndex = 0) {
   print.push(node[0]);
 
   if (node[1] !== -1) {
-    print.push(...traversePreOrder(tree, node[1]));
+    print.push(...recursiveTraversePreOrder(tree, node[1]));
   }
 
   if (node[2] !== -1) {
-    print.push(...traversePreOrder(tree, node[2]));
+    print.push(...recursiveTraversePreOrder(tree, node[2]));
   }
 
   return print;
 }
 
-function traversePostOrder(tree, nodeIndex = 0) {
+function iteractiveTraversePostOrder(tree) {
+  const queue = [tree[0]];
+  const visitOrder = [];
+  const print = [];
+
+  let node;
+
+  while (queue.length) {
+    node = queue.pop();
+
+    if (node[1] !== -1) {
+      queue.push(tree[node[1]]);
+    }
+
+    if (node[2] !== -1) {
+      queue.push(tree[node[2]]);
+    }
+
+    visitOrder.push(node);
+   }
+
+   while (visitOrder.length) {
+     node = visitOrder.pop();
+     print.push(node[0]);
+   }
+
+   return print;
+}
+
+function recursiveTraversePostOrder(tree, nodeIndex = 0) {
   if (!tree) {
-    return;
+    return [];
   }
 
   const print = [];
   const node = tree[nodeIndex];
 
   if (node[1] !== -1) {
-    print.push(...traversePostOrder(tree, node[1]));
+    print.push(...recursiveTraversePostOrder(tree, node[1]));
   }
 
   if (node[2] !== -1) {
-    print.push(...traversePostOrder(tree, node[2]));
+    print.push(...recursiveTraversePostOrder(tree, node[2]));
   }
 
   print.push(node[0]);
@@ -139,13 +214,29 @@ function traversePostOrder(tree, nodeIndex = 0) {
 function traverseTree(tree) {
   const print = [];
 
-  print.push(traverseInOrder(tree));
-  print.push(traversePreOrder(tree));
-  print.push(traversePostOrder(tree));
+  print.push(iteractiveTraverseInOrder(tree));
+  print.push(iteractiveTraversePreOrder(tree));
+  print.push(iteractiveTraversePostOrder(tree));
 
   return print;
 }
 
+function testFullLeftHeight() {
+  const tree = [];
+  const HEIGHT = 10000;
+
+  for (let i = 1; i < HEIGHT; i++) {
+    tree.push([i, i === HEIGHT - 1 ? -1 : i, -1]);
+  }
+
+  const results = traverseTree(tree);
+
+  results.forEach(result => process.stdout.write(`${result.join(' ')}\n`));
+
+  process.exit();
+}
+
 readLines();
+// testFullLeftHeight();
 
 module.exports = traverseTree;
