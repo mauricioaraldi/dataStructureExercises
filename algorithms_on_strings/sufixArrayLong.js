515b66d3f52fe329bafe20389d5e9bcd37efc48d
BuildTools <unconfigured@null.spigotmc.org>
2023-05-04 11:06:32 +0200

    Implementing suffix array long

--- algorithms_on_strings/sufixArrayLong.js
+++ algorithms_on_strings/sufixArrayLong.js
@@ -18,6 +18,99 @@ const readLines = () => {
   });
 };
 
+function sortCharacters(text) {
+  const count = {};
+  let order = 0;
+
+  for (const char of text) {
+    if (!count[char]) {
+      count[char] = {
+        repetition: 0,
+        order: order++,
+      };
+    }
+
+    count[char].repetition++;
+  };
+
+  return Object.keys(count).sort((a, b) => {
+    if (count[a].repetition > count[b].repetition) {
+      return 1;
+    }
+
+    if (count[a].repetition < count[b].repetition) {
+      return -1;
+    }
+
+    if (count[a].repetition === count[b].repetition) {
+      return count[a].order - count[b].order;
+    }
+
+    return 0;
+  });
+}
+
+function computeCharClasses(text, order) {}
+
+function sortDoubled(text, cycleLength, order, classes) {
+  const count = new Array(text.length).fill(0);
+  const newOrder = new Array(text.length);
+
+  for (let i = 0; i < text.length; i++) {
+    count[classes[i]] = count[classes[i]] + 1;
+  }
+
+  for (let i = 1; i < text.length; i++) {
+    count[classes[i]] = count[classes[i]] + 1;
+  }
+
+  for (let i = text.length - 1; i >= 0; i--) {
+    const start = (order[i] - cycleLength + text.length) % text.length;
+    const cl = classes[start];
+
+    count[cl] = count[cl] - 1;
+    newOrder[count[cl]] = start;
+  }
+
+  return newOrder;
+}
+
+function updateClasses(newOrder, classes, cycleLength) {
+  const newClasses = new Array(newOrder.length);
+
+  newClasses[newOrder[0]] = 0;
+
+  for (let i = 1; i < newOrder.length; i++) {
+    const cur = newOrder[i];
+    const prev = newOrder[i - 1];
+    const mid = (cur + cycleLength);
+    const midPrev = (prev + cycleLength) % newOrder.length;
+
+    if (classes[cur] !== classes[prev] || classes[mid] !== classes[midPrev]) {
+      newClasses[cur] = newClasses[prev] + 1;
+    } else {
+      newClasses[cur] = newClasses[prev];
+    }
+  }
+
+  return newClasses;
+}
+
+function buildSuffixArray(text) {
+  const order = sortCharacters(text);
+  const classes = computeCharClasses(text, order);
+
+  const cycleLength = 1;
+
+  while (cycleLength < text.length) {
+    order = sortDoubled(text, cycleLength, order, classes);
+    classes = updateClasses(order, classes, cycleLength);
+    cycleLength = 2 * cycleLength;
+  }
+
+  return order;
+}
+
 function suffix(text) {
   return '';
 }
