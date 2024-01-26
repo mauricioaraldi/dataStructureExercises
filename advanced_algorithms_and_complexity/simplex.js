46e1b31f8f5637fa504bee508dde075a29ec3308
BuildTools <unconfigured@null.spigotmc.org>
2024-01-27 00:22:57 +0100

    Passing test 4 of simplex

--- advanced_algorithms_and_complexity/simplex.js
+++ advanced_algorithms_and_complexity/simplex.js
@@ -29,12 +29,12 @@ const rl = readline.createInterface({
 const readLines = (asXML) => {
   process.stdin.setEncoding('utf8');
 
-  const allCoefficients = []
+  const coefficients = []
 
   rl.once('line', line => {
     let [restrictionsQt, dishesQt] = line.toString().split(' ').map(v => parseInt(v, 10));
 
-    let n = restrictionsQt + 1;
+    let n = restrictionsQt;
 
     if (n === 0) {
       process.stdout.write('');
@@ -42,19 +42,23 @@ const readLines = (asXML) => {
     }
 
     const readCoefficients = line => {
-      const coefficients = line.toString().split(' ').map(v => parseInt(v, 10));
+      const coefficient = line.toString().split(' ').map(v => parseInt(v, 10));
 
-      allCoefficients.push(coefficients);
+      coefficients.push(coefficient);
 
       if (!--n) {
         rl.removeListener('line', readCoefficients);
 
         rl.once('line', line => {
+          const rightHand = line.toString().split(' ').map(v => parseInt(v, 10));
+
+          rl.once('line', line => {
           const pleasures = line.toString().split(' ').map(v => parseInt(v, 10));
 
-          process.stdout.write(calculateDiet(allCoefficients, pleasures).join('\n'));
+            process.stdout.write(calculateDiet(coefficients, rightHand, pleasures).join('\n'));
 
-          process.exit();
+            process.exit();
+          });
         });
       }
     };
@@ -63,118 +67,31 @@ const readLines = (asXML) => {
   });
 };
 
-function getLeftmostNonZeroRowIndex(matrix, pivot) {
-  for (let column = 0; column < matrix[0].length - 2; column++) {
-    for (let row = 0; row < matrix.length; row++) {
-      if (matrix[row][matrix[row].length - 1] === 0 && matrix[row][column]) {
-        return { row, column };
-      }
-    }
-  }
+function normalizePivotRow(matrix, pivotRow, pivotColumn) {
+  const divisor = matrix[pivotRow][pivotColumn];
 
-  return null;
-}
-
-function normalizePivotRow(matrix, pivotColumn) {
-  const divisor = matrix[0][pivotColumn];
-
-  for (let i = 0; i < matrix[0].length - 1; i++) {
-    matrix[0][i] = matrix[0][i] / divisor;
-  }
-
-  if (matrix[0][pivotColumn] !== 1) {
-    normalizePivotRow(matrix, pivotColumn);
+  for (let i = 0; i < matrix[pivotRow].length; i++) {
+    matrix[pivotRow][i] = matrix[pivotRow][i] / divisor;
   }
 }
-let maxLoop = 0;
-function normalizePivotColumn(matrix, pivotColumn) {
-  let shouldNormalize = false;
 
-  console.log(111111, pivotColumn, matrix);
-
-  for (let i = 1; i < matrix.length; i++) {
-    console.log(matrix[i][pivotColumn]);
-    if (matrix[i][pivotColumn]) {
-      shouldNormalize = true;
-      break;
-    };
-
-    console.log(12121212);
-  }
-
-  if (++maxLoop === 5) {
-    return;
-  }
-
-  console.log(222222);
-
-  if (!shouldNormalize) {
-    return;
-  }
-
-  console.log(2333333);
-
-  for (let i = 1; i < matrix.length; i++) {
-    if (matrix[i][pivotColumn] === 0) {
+function normalizePivotColumn(matrix, pivotRow, pivotColumn) {
+  for (let i = 0; i < matrix.length; i++) {
+    if (matrix[i][pivotColumn] === 0 || i === pivotRow) {
       continue;
     }
 
     const shouldAdd = matrix[i][pivotColumn] < 0;
     const multiplier = Math.abs(matrix[i][pivotColumn]);
 
-    for (let j = 0; j < matrix[i].length - 1; j++) {
+    for (let j = 0; j < matrix[i].length; j++) {
       if (shouldAdd) {
-        matrix[i][j] += (matrix[0][j] * multiplier);
+        matrix[i][j] += (matrix[pivotRow][j] * multiplier);
       } else {
-        matrix[i][j] -= (matrix[0][j] * multiplier);
+        matrix[i][j] -= (matrix[pivotRow][j] * multiplier);
       }
     }
   }
-
-  console.log(444444);
-
-  return normalizePivotColumn(matrix, pivotColumn);
-}
-
-function swapArrayRows(matrix, initial, final) {
-  const initialContent = matrix[initial];
-
-  matrix[initial] = matrix[final];
-  matrix[final] = initialContent;
-}
-
-function rowReduce(matrix, currentColumn) {
-  const { row: pivotRow, column: pivotColumn } = getLeftmostNonZeroRowIndex(matrix, currentColumn);
-
-  matrix[pivotRow][matrix[pivotRow].length - 1] = 1;
-
-  swapArrayRows(matrix, 0, pivotRow);
-
-  if (matrix[pivotRow][pivotColumn] !== 1) {
-    normalizePivotRow(matrix, pivotColumn);
-  }
-
-  normalizePivotColumn(matrix, pivotColumn);
-}
-
-function reorder(matrix) {
-  for (let column = 0; column < matrix[0].length - 2; column++) {
-    for (let row = 0; row < matrix.length; row++) {
-      if (matrix[row][column] && row !== column) {
-        swapArrayRows(matrix, column, row);
-
-        return reorder(matrix);
-      }
-    }
-  }
-}
-
-function gaussianElimination(matrix) {
-  for (let i = 0; i < matrix[0].length - 2; i++) {
-    rowReduce(matrix, i);
-  }
-
-  reorder(matrix);
 }
 
 function toPrecision(number) {
@@ -184,32 +101,122 @@ function toPrecision(number) {
   return Number(strNumber.slice(0, decimalPointIndex + 7)).toFixed(6);
 }
 
+function equalizeInequalities(coefficients) {
+  for (let i = 0; i < coefficients.length; i++) {
+    coefficients[i].push(...Array(i).fill(0), 1, ...Array(coefficients.length - i).fill(0));
+  }
+}
 
-function calculateDiet(coefficients, pleasures) {
+/**
+ * Gets the smallest value from last row
+ */
+function identifyPivotColumn(matrix) {
+  const lastRow = matrix[matrix.length - 1];
+  let pivotColumn = undefined;
+
+  for (let column = 0; column < lastRow.length; column++) {
+    if (
+      lastRow[column] < 0
+      && (pivotColumn === undefined || lastRow[column] < lastRow[pivotColumn])
+    ) {
+      pivotColumn = column;
+    }
+  }
+
+  return pivotColumn;
+}
+
+function identifyPivotRow(matrix, pivotColumn) {
+  const pivotRow = {
+    value: undefined,
+    index: undefined,
+  };
+
+  for (let row = 0; row < matrix.length - 1; row++) {
+    const matrixRow = matrix[row];
+    const value = matrixRow[matrixRow.length - 1] / matrixRow[pivotColumn];
+
+    if (pivotRow.index === undefined || value < pivotRow.value) {
+      pivotRow.value = value;
+      pivotRow.index = row;
+    }
+  }
+
+  return pivotRow.index;
+}
+
+function simplex(matrix) {
+  const pivotColumn = identifyPivotColumn(matrix);
+
+  if (pivotColumn === undefined) {
+    return;
+  }
+
+  const pivotRow = identifyPivotRow(matrix, pivotColumn);
+
+  normalizePivotRow(matrix, pivotRow, pivotColumn);
+  normalizePivotColumn(matrix, pivotRow, pivotColumn);
+
+  return simplex(matrix);
+}
+
+function buildResult(matrix) {
+  const result = [];
+
+  for (let column = 0; column < matrix[0].length - 1; column++) {
+    const columnValues = [];
+    let rowIndex = undefined;
+
+    for (let row = 0; row < matrix.length; row++) {
+      if (matrix[row][column] !== 0) {
+        columnValues.push(matrix[row][column]);
+        rowIndex = row;
+      }
+    }
+
+    if (Number(columnValues.join('')) === 1) {
+      result.push(matrix[rowIndex][matrix[0].length - 1]);
+    }
+
+    rowIndex = undefined;
+  }
+
+  return result;
+}
+
+function calculateDiet(coefficients, rightHand, pleasures) {
   if (!coefficients || !coefficients.length) {
+    console.error("Invalid input: No coefficients");
     return ['No solution'];
   }
 
-  for (let i = 0; i < coefficients.length - 1; i++) {
-    coefficients[i].push(coefficients[coefficients.length - 1][i]);
-    coefficients[i].push(0);
+  for (let i = 0; i < coefficients.length; i++) {
+    if (coefficients[i].length !== pleasures.length) {
+      console.error("Invalid input: The number of coefficients in each row of coefficients must match the number of pleasures");
+      return ['No solution'];
+    }
   }
 
-  coefficients.pop();
-
-  console.log(coefficients);
-
-  gaussianElimination(coefficients);
-
-  const result = [];
-
-  for (let i = 0; i < dishes.length; i++) {
-    result.push(toPrecision(dishes[i][dishes[i].length - 2]));
+  if (coefficients.length !== rightHand.length) {
+    console.error("Invalid input: The number of rows in coefficients must match the length of rightHand");
+    return ['No solution'];
   }
 
-  console.log(results);
+  equalizeInequalities(coefficients);
 
-  return ['...', '...'];
+  const table = [];
+
+  for (let i = 0; i <= coefficients.length - 1; i++) {
+    table.push([...coefficients[i], rightHand[i]]);
+  }
+
+  table.push([...pleasures.map(v => -v), ...Array(table[0].length - pleasures.length - 2).fill(0), 1, 0]);
+
+  simplex(table);
+
+  const result = buildResult(table);
+
+  return ['Bounded solution', result.map(v => v.toFixed(15)).join(' ')];
 }
 
 function test(onlyTest) {
@@ -221,8 +228,8 @@ function test(onlyTest) {
           [-1, -1],
           [1, 0],
           [0, 1],
-          [-1, 2, 2],
         ],
+        [-1, 2, 2],
         [-1, 2],
       ),
       expected: 'Bounded solution 0.000000000000000 2.000000000000000',
@@ -233,8 +240,8 @@ function test(onlyTest) {
         [
           [1, 1],
           [-1, -1],
-          [1, -2],
         ],
+        [1, -2],
         [1, 1],
       ),
       expected: 'No solution',
@@ -244,11 +251,23 @@ function test(onlyTest) {
       run: () => calculateDiet(
         [
           [0, 0, 1],
-          [3],
         ],
+        [3],
         [1, 1, 1],
       ),
       expected: 'Infinity',
+    },
+    {
+      id: 4,
+      run: () => calculateDiet(
+        [
+          [1, 1],
+          [2, 1],
+        ],
+        [12, 16],
+        [40, 30],
+      ),
+      expected: 'Bounded solution 4.000000000000000 8.000000000000000 400.000000000000000',
     },
   ];
 
